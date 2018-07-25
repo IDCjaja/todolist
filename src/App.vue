41f8f7579b7491620a136d81499b0f44a65551bc
limengjia <http://en.mail.qq.com/cgi-bin/frame_html?sid=ybETBwFeFBUnOQtR&r=3baf7e1446eff88e726ff3579b3a65fb>
2018-07-25 15:05:03 +0800

    Submit files

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <input v-model="newItem" v-on:keyup.enter="addNew">
+    <ul>
+      <li v-for="(item,index) in items"
+        :key="index"
+        :class="{finished: item.isFinished}"
+        v-on:click="toggleFinish(item)">
+        {{ item.label }}
+      </li>
+    </ul>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      title: 'this is a todo list',
+      items: [],
+      newItem: ''
+    }
+  },
+  methods: {
+    toggleFinish: function(item) {
+      item.isFinished = !item.isFinished
+    },
+    addNew() {
+      this.items.push({
+        label: this.newItem,
+        isFinished: false
+      })
+      this.newItem = ''
+    }
+  }
+}
+</script>
+
+<style>
+.finished {
+  text-decoration: underline;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
